1cc3d5d26554b0286b6bf9fd1ad6653df9dbaa87
kraksin <kraksin12@gmail.com>
2020-09-15 11:46:43 +0900

    cut summary

--- src/Movie.js
+++ src/Movie.js
@@ -9,7 +9,7 @@ function Movie({ year, rating, title, summary, poster, genres }) {
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h3 className="movie__year">{year}</h3>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 140)} ...</p>
                 <ul className="movie__genres">
                     {genres.map((genre, index) => 
                         <li key={index} className="movie__genre">{genre}</li>
