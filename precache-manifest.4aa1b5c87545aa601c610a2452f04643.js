b4ca6c6f350c32fc03b714a3b5b21ce1269cf021
kraksin <kraksin12@gmail.com>
2020-09-15 12:04:23 +0900

    Updates

---
+++ precache-manifest.4aa1b5c87545aa601c610a2452f04643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff72061ca13aef555c230d4ae07de3cf",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9d43b3d7b5a78a0e8649",
+    "url": "/movie_app/static/css/main.f8778d70.chunk.css"
+  },
+  {
+    "revision": "890fdb3298edb943bfa3",
+    "url": "/movie_app/static/js/2.bb9b8681.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.bb9b8681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d43b3d7b5a78a0e8649",
+    "url": "/movie_app/static/js/main.0108af23.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
